d7554421a628b29b544863575a8e172046e91fe2
duness <dness@dness.de>
2021-12-13 19:44:06 +0100

    test

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
 $plugin->component = 'local_isymeta';
-$plugin->version = 2021101512;
+$plugin->version = 2021101513;
 $plugin->requires = 2010112400;
 $plugin->dependencies = array('block_isymetaselect' => 2021083006);
