8eb2dbd46f044ace1a8b1494f2335ac1e9f93732
tinjohn <tina.john@th-luebeck.de>
2022-12-08 20:27:52 +0100

    Bug fix php 8.0 Moodle 4.0.4
    Error thrown: mysqli::real_escape_string(): Argument #1 ($string) must be of type string, array given
    when try to open settings from within the course

--- edit_metadata.php
+++ edit_metadata.php
@@ -389,7 +389,10 @@ if ($mform->is_cancelled()) {
 
         $toform->coursetitle = $course->fullname;
         if (isset($course->summary)) {
-            $toform->teasertext['text'] = $course->summary;
+          // CHANGED tinjohn 20221208 given array is not valid and not necessary.
+          // Modified to single string.
+          // Error php 8.0 and Moodle 4.0.4 -> mysqli::real_escape_string(): Argument #1 ($string) must be of type string, array given.
+            $toform->teasertext = $course->summary;
         }
         if (isset($course->startdate)) {
             $toform->starttime = $course->startdate;
